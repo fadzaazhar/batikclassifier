34b308e2463b865f11f8997c1294c179d95eb403
anissaazhari <45798171+anissaazhari@users.noreply.github.com>
2024-07-03 14:52:24 +0700

    Update batik.py

--- batik.py
+++ batik.py
@@ -5,43 +5,9 @@ import tensorflow as tf
 import cv2
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 import os
-import requests
 
-def download_file_from_google_drive(file_id, destination):
-    URL = "https://drive.google.com/uc?export=download"
-    
-    session = requests.Session()
-    response = session.get(URL, params={'id': file_id}, stream=True)
-    token = get_confirm_token(response)
-    
-    if token:
-        params = {'id': file_id, 'confirm': token}
-        response = session.get(URL, params=params, stream=True)
-    
-    save_response_content(response, destination)
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-    return None
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk:  # filter out keep-alive new chunks
-                f.write(chunk)
-
-# Ganti 'YOUR_GOOGLE_DRIVE_FILE_ID' dengan File ID yang Anda dapatkan
-file_id = '17xPnuufojPGJsfp4iMyihzr3Xnn9VBb8'
-destination = 'batik_model.h5'
-download_file_from_google_drive(file_id, destination)
-
-# Memuat model
-import tensorflow as tf
-model = tf.keras.models.load_model(destination)
+# Load the pre-trained model
+model = tf.keras.models.load_model('batik_model.h5')
 
 # Class names dictionary
 class_names = {
